1206e678349477b1174f586f60ca67e8488367ac
Suman Mahato <103480485+Suman659@users.noreply.github.com>
2023-09-24 21:49:40 +0530

    Create class1.js

---
+++ class1.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log(typeof("Hello World"));
+var pie = "3.14";
+console.log(pie);
+
+// after using const we cannot change the value for variable
+const pi = "3.14"
